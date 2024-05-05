5c5788820629e3aea3e788cc24495781dca802d5
ZhiWei0731 <william.liu0731@gmail.com>
2024-05-05 21:29:10 +0800

    Feat: add database variable.
    Add constant variable of users and records

---
+++ src/main/java/com/jeepway/constant/RecordConstants.java
@@ -0,0 +1,13 @@
+package com.jeepway.constant;
+
+public class RecordConstants {
+    public static String TABLE_NAME = "records";
+    
+    public static String COLUMN_RECORDID = "record_id";
+    public static String COLUMN_USERID = "user_id";
+	public static String COLUMN_CATEGORY = "category";
+	public static String COLUMN_DATE = "date";
+	public static String COLUMN_AMOUNT = "amount";
+	public static String COLUMN_TYPE = "type";
+    public static String COLUMN_DETAIL = "detail";
+}
---
+++ src/main/java/com/jeepway/constant/UserConstants.java
@@ -0,0 +1,11 @@
+package com.jeepway.constant;
+
+public class UserConstants {
+    public static String TABLE_NAME = "users";
+
+    public static String COLUMN_USERID = "uid";
+	public static String COLUMN_NAME = "name";
+	public static String COLUMN_EMAIL = "email";
+	public static String COLUMN_PHONE = "phone";
+	public static String COLUMN_PASSWORD = "password";
+}
